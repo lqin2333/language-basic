aa15e1f2c33e8a3a43d8723bb6f8cc43dc34f908
ryanrunqin <qinrun1989@gmail.com>
2018-01-17 18:16:07 +1300

    added angular code

--- JS/drag.js
+++ JS/drag.js
@@ -1,42 +1,43 @@
 function Drag(id){
-				var _this = this;
-				this.disX = 0;
-				this.disY = 0;
+	var _this = this;
+	this.disX = 0;
+	this.disY = 0;
 
-				this.oDiv = document.getElementById(id);
+	this.oDiv = document.getElementById(id);
 
-				//BE CAREFUL:
-				//this.oDiv.onmousedown = this.fnDown(ev)
-				//will not work here 
-				//because the "this" is different
-				//that's why using "_this" here
-				this.oDiv.onmousedown = function(ev){
-					_this.fnMouseDown(ev);
-				}
-			}
+	//BE CAREFUL:
+	//this.oDiv.onmousedown = this.fnDown(ev)
+	//will not work here 
+	//because the "this" is different
+	//that's why using "_this" here
+	this.oDiv.onmousedown = function(ev){
+		_this.fnMouseDown(ev);
+		return false; //prevent browser event
+	}
+}
 
-			Drag.prototype.fnMouseDown = function(ev){
-				var _this = this;
-				var oEvent = ev || event;
-				this.disX = oEvent.clientX - this.oDiv.offsetLeft;
-				this.disY = oEvent.clientY - this.oDiv.offsetTop;
+Drag.prototype.fnMouseDown = function(ev){
+	var _this = this;
+	var oEvent = ev || event;
+	this.disX = oEvent.clientX - this.oDiv.offsetLeft;
+	this.disY = oEvent.clientY - this.oDiv.offsetTop;
 
-				//The same reason as above (this and _this)
-				document.onmousemove = function(ev){
-					_this.fnMouseMove(ev);
-				}
-				document.onmouseup = function(ev){
-					_this.fnMouseUp(ev);
-				}
-			}
+	//The same reason as above (this and _this)
+	document.onmousemove = function(ev){
+		_this.fnMouseMove(ev);
+	}
+	document.onmouseup = function(ev){
+		_this.fnMouseUp(ev);
+	}
+}
 
-			Drag.prototype.fnMouseMove = function(ev){
-				var oEvent = ev || event;
-				this.oDiv.style.left = oEvent.clientX - this.disX + 'px';
-				this.oDiv.style.top = oEvent.clientY - this.disY + 'px';
-			}
+Drag.prototype.fnMouseMove = function(ev){
+	var oEvent = ev || event;
+	this.oDiv.style.left = oEvent.clientX - this.disX + 'px';
+	this.oDiv.style.top = oEvent.clientY - this.disY + 'px';
+}
 
-			Drag.prototype.fnMouseUp = function(){
-				document.onmousemove = null;
-				document.onmouseup = null;
-			}
+Drag.prototype.fnMouseUp = function(){
+	document.onmousemove = null;
+	document.onmouseup = null;
+}
